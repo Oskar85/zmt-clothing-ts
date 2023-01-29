12614d3a5fe90551dae5eabd60725be2300b6e84
Alex <alexbal2010@gmail.com>
2023-01-29 14:33:01 +0200

    first commit here

---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import CategoryItem from "../category-item/category-item.component";
+import './directory.style.scss'
+import { ICategoriesProps } from "../../types/ICategories.types";
+
+
+const Directory: FC<ICategoriesProps> = ( { categories } ) => {
+    console.log( categories )
+    return (
+        <div className="directory-container">
+            {
+                categories.map( ( category ) => (
+                    <CategoryItem category={ category } key={ category.id }/>
+                ) )
+            }
+        </div>
+    )
+};
+
+export default Directory;
---
+++ src/types/ICategories.types.ts
@@ -0,0 +1,9 @@
+export interface ICategories {
+    id: number
+    title: string
+    imageUrl: string
+}
+
+export interface ICategoriesProps {
+    categories: ICategories[]
+}
